4f27680a88cc44630cf5e4b6dbd4ec2625e953e4
Jordan Heemskerk <jordanh@shaw.ca>
2014-03-05 18:44:38 -0800

    Clean up code
    Remove some code that was added automatically
    upon project creation.

--- AudioBookBeta/MainPage.xaml.cs
+++ AudioBookBeta/MainPage.xaml.cs
@@ -240,20 +240,5 @@ namespace AudioBookBeta
             BackgroundAudioPlayer.Instance.Position = BackgroundAudioPlayer.Instance.Position + interval;
             App.player.selectedBook.setPosition((int)BackgroundAudioPlayer.Instance.Position.TotalSeconds);
         }
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
     }
 }
